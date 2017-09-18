7bdabc5a973f308a1448e3147f07c15e65c44a84
Peter Levinsky <pele@easj.dk>
2017-09-18 14:22:12 +0200

    Add project files.

---
+++ ClassDemoHTTPServer/HTTPServer.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ClassDemoHTTPServer
+{
+    public class HTTPServer
+    {
+        private readonly int _port;
+
+        public HTTPServer(int port)
+        {
+            this._port = port;
+        }
+
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, _port);
+            server.Start();
+            Trace.TraceInformation("Server started on " + _port);
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Trace.TraceInformation("New Client");
+                Task.Run(() =>
+                    {
+                        TcpClient socket = client;
+                        DoClient(client);
+                    }
+                );
+            }
+        }
+
+        private void DoClient(TcpClient client)
+        {
+            // do something
+
+            Trace.Flush();
+        }
+    }
+}
---
+++ ClassDemoHTTPServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ClassDemoHTTPServer
+{
+    class Program
+    {
+        private const int port = 7;
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+            Trace.Listeners.Add(new TextWriterTraceListener(new StreamWriter(@"m:\intpub\hhtpServer.log")));
+
+            HTTPServer server = new HTTPServer(port);
+            server.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
